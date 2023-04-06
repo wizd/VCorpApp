759014c99ce6940d57b709c90070ae11be0b23e0
wizd <rcvbuf@gmail.com>
2023-04-07 00:52:12 +0800

    fix

--- src/components/tools/ExpandableView.tsx
+++ src/components/tools/ExpandableView.tsx
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: 24,
     alignContent: 'center',
     alignItems: 'center',
   },
