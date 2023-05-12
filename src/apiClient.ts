aade90c158cb03c95bb042f7cb5e99b6110b27e8
wizd <rcvbuf@gmail.com>
2023-05-12 15:36:30 +0800

    clean code

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -1,9 +1,9 @@
 import axios, {AxiosInstance} from 'axios';
 
-const OPENAI_KEY = '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';
+const OPENAI_KEY = 'sk-...';
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'https://ai.lyra.live/v1',
+  baseURL: 'https://api.openai.com/v1',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${OPENAI_KEY}`,
