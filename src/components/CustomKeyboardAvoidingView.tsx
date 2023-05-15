1f73e09f31d0f2a5c1a79e1676db254923fb71c1
wizard <rcvbuf@gmail.com>
2023-05-15 17:34:23 +0800

    fix input on iphone 7

--- src/components/CustomKeyboardAvoidingView.tsx
+++ src/components/CustomKeyboardAvoidingView.tsx
@@ -3,6 +3,7 @@ import {useHeaderHeight} from '@react-navigation/elements';
 import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
 import React from 'react';
 
+// add 20 on iphone 7 to rise input when keyboard poped up
 export function CustomKeyboardAvoidingView({children, style}: any) {
   const insets = useSafeAreaInsets();
   const headerHeight = useHeaderHeight();
@@ -12,7 +13,7 @@ export function CustomKeyboardAvoidingView({children, style}: any) {
       style={StyleSheet.compose(style, {marginBottom: insets.bottom})}
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       enabled={true}
-      keyboardVerticalOffset={headerHeight + insets.bottom}>
+      keyboardVerticalOffset={headerHeight + insets.bottom + 20}>
       {children}
     </KeyboardAvoidingView>
   );
