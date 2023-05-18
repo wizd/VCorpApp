8b38b936ab027c62b2be12b4dc08ae545364b8cb
wizd <rcvbuf@gmail.com>
2023-05-18 15:29:33 +0800

    better fix of module react-native-audio-record

---
+++ scripts/copy-modules.js
@@ -0,0 +1,22 @@
+// copy-modules.js
+var fs = require('fs');
+var path = require('path');
+
+var srcDir = path.resolve(__dirname, './../pkgs/react-native-audio-record/ios');
+var destDir = path.resolve(
+  __dirname,
+  './node_modules/react-native-audio-record/ios',
+);
+
+fs.readdir(srcDir, function (err, files) {
+  if (err) {
+    return console.error('Could not list the directory.', err);
+  }
+
+  files.forEach(function (file, index) {
+    var srcFile = path.join(srcDir, file);
+    var destFile = path.join(destDir, file);
+
+    fs.copyFileSync(srcFile, destFile);
+  });
+});
