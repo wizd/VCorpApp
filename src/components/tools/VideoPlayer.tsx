745a6e3c1d6e895a435716477c1e665d000a773a
wizd <rcvbuf@gmail.com>
2023-06-08 21:30:56 +0800

    fix video player

--- src/components/tools/VideoPlayer.tsx
+++ src/components/tools/VideoPlayer.tsx
@@ -37,8 +37,10 @@ const VideoPlayer = ({videoUrl}: {videoUrl: string}) => {
         style={styles.video}
         resizeMode="contain"
         paused={paused}
+        fullscreen={isFullScreen}
+        onLoad={() => setPaused(true)}
       />
-      <View>
+      <View style={{flexDirection: 'row'}}>
         <SmallButton
           iconName={paused ? 'play-arrow' : 'pause'}
           onPress={togglePlayPause}
@@ -83,6 +85,13 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
+  },
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   },
 });
 
